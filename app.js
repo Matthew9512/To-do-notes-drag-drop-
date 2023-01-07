ff45371255914bf1288fbc5b0f6b5306ead84f59
Matthew9512 <mateuszmaciak95@gmail.com>
2023-01-07 12:52:43 +0100

    new

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const sectionDragArea = document.querySelectorAll('.section__drag-area');
 const stateObject = {
   location: [],
   value: [],
+  noteId: [],
 };
 
 //
@@ -18,7 +19,6 @@ const currentItemData = (e) => {
     stateObject.location = click.closest('.section');
 
     addTask();
-    updateLS();
 
     input.value = '';
   }
@@ -27,13 +27,20 @@ const currentItemData = (e) => {
 //
 const addTask = () => {
   const click = stateObject.location.querySelector('.section__drag-area');
+  console.log(click);
+  // id of the task
+  let id = new Date().getTime().toString();
+  let parentID = stateObject.location.id;
+
+  stateObject.noteId = id;
 
   const newItem = `
-    <div draggable="true" class="section__drag-item">
+    <div draggable="true" class="section__drag-item" data-id="${id}">
       <p class="section__new-task">${stateObject.value}</p>
       <i class="delete fa-solid fa-xmark"></i>
     </div>`;
   click.insertAdjacentHTML('afterbegin', newItem);
+
   const dragItems = document.querySelectorAll('.section__drag-item');
   const btnsDelete = document.querySelectorAll('.delete');
 
@@ -41,9 +48,11 @@ const addTask = () => {
     btn.addEventListener('click', deleteItem);
   });
 
+  updateLS(parentID);
   drag(dragItems);
 };
 
+// ===
 // drag and drop items
 const drag = (dragItems) => {
   dragItems.forEach((item) => {
@@ -62,6 +71,7 @@ const deleteItem = (e) => {
   const parent = e.target.closest('.section__drag-area');
   const target = parent.querySelector('.section__drag-item');
   parent.removeChild(target);
+  removeLS(target);
 };
 
 // place of drag items to be moved
@@ -73,7 +83,7 @@ sectionDragArea.forEach((area) => {
   });
 });
 
-// localStorage
+// === localStorage
 // getLocalStorage
 const getLS = () => {
   const lsArr = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
@@ -81,11 +91,54 @@ const getLS = () => {
 };
 
 // update localStorage
-const updateLS = () => {
+const updateLS = (parentID) => {
   const lsArr = getLS();
-  lsArr.push(stateObject.value);
+
+  const lsObject = {
+    id: stateObject.noteId,
+    value: stateObject.value,
+    location: parentID,
+  };
+
+  lsArr.push(lsObject);
   localStorage.setItem('notes', JSON.stringify(lsArr));
 };
 
-//
+// remove item from localStorage
+const removeLS = (target) => {
+  const lsArr = getLS();
+  const lsItems = lsArr.filter((item) => item.id !== target.dataset.id);
+  localStorage.setItem('notes', JSON.stringify(lsItems));
+};
+
+// === domcontentloaded function
+const loadDisplay = () => {
+  const lsArr = getLS();
+  console.log(lsArr);
+  if (!lsArr.length) return;
+  console.log(`you have some notes in your notes folder`);
+
+  // for (let ls in lsArr) {
+  //   const m = document.querySelector(`#${lsArr[ls].location}`);
+  //   console.log(m);
+  //   m.innerHTML = `<div draggable="true" class="section__drag-item" data-id="${lsArr.at(ls).id}">
+  //   <p class="section__new-task">${lsArr.at(ls).value}</p>
+  //   <i class="delete fa-solid fa-xmark"></i>
+  // </div>`;
+  // }
+
+  // const parent = lsArr.at(1).location;
+  // console.log(parent);
+  // const y = document.querySelector(`#${parent}`);
+  // console.log(y);
+  // const qw = y.querySelector('.section__drag-area');
+  // qw.innerHTML = `
+  // <div draggable="true" class="section__drag-item" data-id="${lsArr.at(0).id}">
+  //   <p class="section__new-task">${lsArr.at(0).value}</p>
+  //   <i class="delete fa-solid fa-xmark"></i>
+  // </div>`;
+};
+
+//addEventListeners
 sectionsContainer.addEventListener('click', currentItemData);
+document.addEventListener('DOMContentLoaded', loadDisplay);
